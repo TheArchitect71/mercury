8f206af24fc2f1d3f0408b0b281fe219121b46c2
Erick Flores <thearchitect@MacBook-Air.local>
2020-07-19 11:42:09 -0500

    routing is not being used

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+
 
 const routes: Routes = [];
 
